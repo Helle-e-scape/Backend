4687b3a2cca8bfda83b3bafd9bc6dd7e75c74970
Lucas Goncalves <87122664+RuKaSuu@users.noreply.github.com>
2024-10-15 15:37:52 +0200

    feat : Creating the creation of the room

--- src/route.js
+++ src/route.js
@@ -1,7 +1,9 @@
 const authRoutes = require("./routes/auth");
+const roomRoutes = require("./routes/room");
 const router = require("express").Router();
 
 router.use("/auth", authRoutes);
+router.use("/room", roomRoutes);
 
 router.get("/", (req, res) => {
   res.send("Welcome to the Hell(e)scape App API");
