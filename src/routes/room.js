9aa233a7face69046432220ed4f02258ca2d50c2
Torres Hugo <torresmartins.hugo@gmail.com>
2024-10-17 09:35:05 +0200

    replace POST with PUT in routes/room

--- src/routes/room.js
+++ src/routes/room.js
@@ -2,6 +2,6 @@ const route = require("express").Router();
 const RoomController = require("../controllers/room");
 
 route.post("/create", RoomController.create);
-route.post("/userJoin", RoomController.userJoin);
+route.put("/userJoin", RoomController.userJoin);
 
 module.exports = route;
