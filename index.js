5b46eb3cfad799c0a254f2108636136eb93b2c1f
Lucas Goncalves <87122664+RuKaSuu@users.noreply.github.com>
2024-10-18 10:30:34 +0200

    feat : setup websocket to talk with the front and unity

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const cors = require("cors");
 const connectDB = require("./src/db/connect");
 const routes = require("./src/route");
 const port = 3000;
-
 const app = express();
-
+const server = require("http").createServer(app);
+const { initWebSocket } = require("./src/websocket");
 connectDB(process.env.DATABASE_URL);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+initWebSocket();
+
 app.use("/", routes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
